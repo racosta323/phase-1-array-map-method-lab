6c1f0bb22680ba732a6b0bd558c63671eda9cd0e
Rene <racosta323@gmail.com>
2024-01-20 12:22:22 -0600

    lab complete

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+
+
+
+
+
+function titleCased(){
+  let eachTitle = tutorials.map(title=>title.split(' ').map(word =>{
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }).join(' ')) 
+
+  return eachTitle
 }
+    
+
+
+
+
+
+
+
+
